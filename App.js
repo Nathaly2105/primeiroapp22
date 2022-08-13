c7f5891c8900d64f7acd88c0fcd0c3fb73b73fa1
Nathaly2105 <nathaly.rocha.est@gmail.com>
2022-08-13 01:42:10 +0000

    criei uma calculadora

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React from 'react';
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oi Layne</Text>
-      <StatusBar style="auto" />
+		  <View style={styles.bloco}>
+		  	<Text style={styles.titulo}>Calculadora Simples</Text>
+		  </View>
+		  <br></br>
+		  <View style={styles.bloco}>
+		  	<Text> Valor 1: </Text>
+			 <TextInput style={styles.input}/>
+		  </View>
+		  <br></br>
+		  <View style={styles.bloco}>
+		  	<Text> Valor 2: </Text>
+			<TextInput style={styles.input}/>
+		  </View>
+		  <br></br>
+		  <View>
+		  	<TouchableOpacity style={styles.botao}>
+				<Text style={styles.textoBotao}>SOMAR</Text>  
+			</TouchableOpacity>
+		  </View>
     </View>
   );
 }
@@ -13,8 +30,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	  margin: 20
+	  
   },
+	bloco: {
+		margintop: 20,
+	},
+	titulo: {
+		
+		fontSize: 30,
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	input: {
+		borderWidth:2,
+		fontSize: 20,
+		borderRadius: 5
+		
+	},
+	botao: {
+		backgroundColor:'#000',
+		borderRadius:15,
+		width: 90,
+		marginLeft: 110
+	},
+	textoBotao: {
+		color: '#fff',
+		textAlign: 'center',
+		fontSize: 20
+	}
+	
 });
